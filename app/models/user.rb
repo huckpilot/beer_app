00ba1cc4441af21a2f84dc15388e1c2f8424fafe
huckpilot <huckpilot@gmail.com>
2015-06-02 12:04:23 -0400

    migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :beers
+  
   has_secure_password
   validates :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: {message: "I know you"}
